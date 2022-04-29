e1b523861fa33ee9531defc64f71ae888c763118
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-29 16:57:37 +0300

    Problem solved.New problem added.

---
+++ Inheritence - Exercise/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public const double DefaultFuelConsuption = 1.25;
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+
+        public virtual double FuelConsumption
+            => DefaultFuelConsuption;
+        
+
+        public double Fuel { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public virtual void Drive(double kilomeneters)
+        {
+            //50 - (10 * 4) = 10 litters
+
+            if (this.Fuel - (kilomeneters * this.FuelConsumption) >= 0)
+            {
+                this.Fuel -= (kilomeneters * this.FuelConsumption);
+            }
+        }
+    }
+}
