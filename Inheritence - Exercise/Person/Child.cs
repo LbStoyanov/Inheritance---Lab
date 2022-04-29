77781149fe8f119c9044579ebaf550951a4b262e
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-29 13:02:41 +0300

    Problem solved.

---
+++ Inheritence - Exercise/Person/Child.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    internal class Child : Person
+    {
+        public Child(string name,int age)
+            : base(name,age)
+        {
+
+        }
+    }
+}
