b44bf627e8122b9a7ef76e7087c92f4b2bc7286c
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-05 12:01:56 +0300

    Problem solved.New problem added.

---
+++ Encapsulation - Exercise/01. Class Box Data/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01._Class_Box_Data
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            Box box = new Box(length, width, height);
+            double surfaceArea = box.SurfaceArea();
+            double lateralSurfaceArea = box.LateralSurfaceArea();
+            double volume = box.Volume();
+
+            Console.WriteLine($"Surface Area - {surfaceArea:f2}");
+            Console.WriteLine($"Lateral Surface Area - {lateralSurfaceArea:f2}");
+            Console.WriteLine($"Volume - {volume:f2}");
+            
+        }
+    }
+}
