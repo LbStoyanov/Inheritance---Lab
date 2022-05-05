30dccc404ba7ae6d08f6f9ad9370599a36e3fcf8
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-05 12:46:47 +0300

    Code improved

--- Encapsulation - Exercise/01. Class Box Data/StartUp.cs
+++ Encapsulation - Exercise/01. Class Box Data/StartUp.cs
@@ -10,14 +10,22 @@ namespace _01._Class_Box_Data
             double width = double.Parse(Console.ReadLine());
             double height = double.Parse(Console.ReadLine());
 
-            Box box = new Box(length, width, height);
-            double surfaceArea = box.SurfaceArea();
-            double lateralSurfaceArea = box.LateralSurfaceArea();
-            double volume = box.Volume();
+            try
+            {
+                Box box = new Box(length, width, height);
+                double surfaceArea = box.SurfaceArea();
+                double lateralSurfaceArea = box.LateralSurfaceArea();
+                double volume = box.Volume();
 
-            Console.WriteLine($"Surface Area - {surfaceArea:f2}");
-            Console.WriteLine($"Lateral Surface Area - {lateralSurfaceArea:f2}");
-            Console.WriteLine($"Volume - {volume:f2}");
+                Console.WriteLine($"Surface Area - {surfaceArea:f2}");
+                Console.WriteLine($"Lateral Surface Area - {lateralSurfaceArea:f2}");
+                Console.WriteLine($"Volume - {volume:f2}");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
             
         }
     }
