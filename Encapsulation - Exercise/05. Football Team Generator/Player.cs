37623008a2a269bf31182c495500571ecc495728
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-09 13:31:42 +0300

    Problem resolved at 30%.Improving code.

---
+++ Encapsulation - Exercise/05. Football Team Generator/Player.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EncapsulationExercise
+{
+    public class Player
+    {
+        private readonly List<string> stats = new List<string>
+        {
+            "endurance",
+            "sprint",
+            "dribble",
+            "endurance",
+            "shooting",
+        };
+        private string name;
+        public Player(string name,List<int> stats)
+        {
+            Name = name;
+            Stats = stats;
+        }
+        
+
+        public List<int> Stats
+        {
+            get { return Stats; }
+            private set
+            {
+                
+
+                if (Stats.Any(x => x < 0 || x > 100))
+                {
+
+                    int index = Stats.FindIndex(x => x < 0 || x > 100);
+
+                    throw new ArgumentException($"{stats[index]} should be between 0 and 100.");
+                }
+                Stats = value;
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("A name should not be empty.");
+                }
+                name = value;
+            }
+        }
+
+    }
+}
---
+++ Encapsulation - Exercise/05. Football Team Generator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace EncapsulationExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string commands;
+
+            while ((commands = Console.ReadLine()) != "END")
+            {
+                string[] splittedCommands = commands.Split(";");
+                string action = splittedCommands[0];
+                string teamName = splittedCommands[1];
+                string playerName = splittedCommands[2];
+                var stats = splittedCommands.Skip(3).ToList();
+
+
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
