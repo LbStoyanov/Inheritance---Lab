1145964b086a5e6fbd5ce1f514432fc0029d66ac
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-13 16:14:41 +0300

    Problem 02. Enter Numbers solved.

---
+++ Exceptions and Error Handling - Lab/01. Square Root/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01._Square_Root
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }        
+        }
+    }
+}
---
+++ Exceptions and Error Handling - Lab/02. Enter Numbers/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Enter_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> finalNumbers = new List<int>();
+            int startNumber = 1;
+            int endNumber = 100;
+            int currentNumber = 1;
+            
+            while (finalNumbers.Count < 10)
+            {
+                
+                try
+                {
+                    string input = Console.ReadLine();
+                    
+
+                    if (int.TryParse(input, out currentNumber))
+                    {
+                        if (currentNumber <= startNumber || currentNumber > endNumber)
+                        {
+                            throw new ArgumentException($"Your number is not in range { startNumber } - 100!");
+                        }
+                        else
+                        {
+                            
+                            finalNumbers.Add(currentNumber);
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Invalid Number!");
+                    }
+
+                    startNumber = currentNumber;
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+
+            Console.WriteLine(String.Join(", ",finalNumbers));
+
+        }
+    }
+}
