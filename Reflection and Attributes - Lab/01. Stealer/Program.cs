bc4c2fcfac8e7af02ee5d0737261ef910c4f7b26
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-23 15:32:30 +0300

    Problem 04.Symbol in Matrix solved.

--- Reflection and Attributes - Lab/01. Stealer/Program.cs
+++ Reflection and Attributes - Lab/01. Stealer/Program.cs
@@ -6,7 +6,36 @@ namespace _01._Stealer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int matrixSize = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] rowInput = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < rowInput.Length; col++)
+                {
+                    matrix[row, col] = rowInput[col];
+                }
+            }
+
+            char symbol = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == symbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        return;
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"{symbol} does not occur in the matrix");
+
         }
     }
 }
