eee7367dccf44a49ddbfd790a49c1757ac136273
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-24 12:34:09 +0300

    Problem 02. High Quality Mistakes solved.

--- Reflection and Attributes - Lab/01. Stealer/Spy.cs
+++ Reflection and Attributes - Lab/01. Stealer/Spy.cs
@@ -28,6 +28,34 @@ namespace Stealer
             return stringBuilder.ToString().Trim();
         }
 
-        
+        public string AnalyzeAccessModifiers(string investigatedClass)
+        {
+            Type classType = Type.GetType(investigatedClass);
+
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public);
+
+            MethodInfo[] classPublicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+            MethodInfo[] classNonPublicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (FieldInfo field in classFields)
+            {
+                sb.AppendLine($"{field.Name} must be private!");
+            }
+
+            foreach (MethodInfo method in classNonPublicMethods.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} have to be public!");
+            }
+
+            foreach (MethodInfo method in classPublicMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} have to be private!");
+            }
+
+
+            return sb.ToString().Trim();
+        }
     }
 }
--- Reflection and Attributes - Lab/01. Stealer/StartUp.cs
+++ Reflection and Attributes - Lab/01. Stealer/StartUp.cs
@@ -7,10 +7,7 @@ namespace Stealer
         static void Main(string[] args)
         {
             Spy spy = new Spy();
-            string[] requestedFieldsnames = new string[] { "username", "password" };
-            string result = 
-                spy.StealFieldInfo(investigatedClassName:"Stealer.Hacker",requestedFieldsnames);
-
+            string result = spy.AnalyzeAccessModifiers("Hacker");
             Console.WriteLine(result);
         }
     }
