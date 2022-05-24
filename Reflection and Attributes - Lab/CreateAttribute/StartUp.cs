2c9c5c096ec8fd4de5f2e11af35e2f31cdc7e121
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-24 13:30:45 +0300

    Problem 06. Code Tracker solved.

---
+++ Reflection and Attributes - Lab/CreateAttribute/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using CreateAttribute;
+using System;
+
+namespace AuthorProblem
+{
+    [Author("Lybeti")]
+    public class StartUp
+    {
+        [Author("Peshko")]
+        static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
