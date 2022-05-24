7b0f1c3efaa3aa397700afeb2991def2cc5f5226
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-24 16:29:53 +0300

    Problem 01. Command Pattern solved.

---
+++ Reflection and Attributes - Exercise/01. Command Pattern/ReflectionAndAttributes/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,42 @@
+﻿using CommandPattern.Core.Commands;
+using CommandPattern.Core.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] input = args.Split();
+
+            string commandInput = input[0];
+            string[] value = input.Skip(1).ToArray();
+
+            ICommand command = default;
+
+            Type type = Assembly.GetCallingAssembly().GetTypes().FirstOrDefault(x => x.Name == commandInput + "Command");
+
+            if (type == null)
+            {
+                throw new InvalidOperationException("Missing command");
+            }
+
+            Type commandInterface = type.GetInterface("ICommand");
+
+            if (commandInterface == null)
+            {
+                throw new InvalidOperationException("Not a command");
+            }
+
+            command = Activator.CreateInstance(type) as ICommand;
+         
+
+            string result = command.Execute(value);
+
+            return result;
+        }
+    }
+}
