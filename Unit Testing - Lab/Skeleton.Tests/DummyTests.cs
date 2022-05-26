ea916a32a600e9781c0ee3a1fcca9ae37e9cdf49
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-26 15:06:13 +0300

    Problem 02. Dummy Tests solved

--- Unit Testing - Lab/Skeleton.Tests/DummyTests.cs
+++ Unit Testing - Lab/Skeleton.Tests/DummyTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace Skeleton.Tests
 {
@@ -6,8 +7,46 @@ namespace Skeleton.Tests
     public class DummyTests
     {
         [Test]
-        public void Test1()
+        public void Dummy_Loose_Health_If_Attack()
         {
+            Dummy dummy = new Dummy(10,10);
+
+            dummy.TakeAttack(9);
+
+            Assert.That(dummy.Health, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Dead_Dummy_Throws_An_Exception_If_Attacked()
+        {
+            Dummy dummy = new Dummy(0, 10);
+
+            
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                dummy.TakeAttack(9);
+            });
+        }
+
+        [Test]
+        public void Dead_Dummy_Can_Give_XP()
+        {
+            Dummy dummy = new Dummy(0, 10);
+
+            Assert.That(dummy.GiveExperience, Is.EqualTo(10));
+            
+        }
+
+        [Test]
+        public void Alive_Dummy_Cannnot_Give_XP()
+        {
+            Dummy dummy = new Dummy(100, 10);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                dummy.GiveExperience();
+            });
 
         }
     }
