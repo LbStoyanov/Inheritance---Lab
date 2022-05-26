ffd705e42b325b4811dcf10eac82bc222ba461a7
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-26 09:49:36 +0300

    Problem solved.

---
+++ Reflection and Attributes - Exercise/01. Command Pattern/ReflectionAndAttributes/ValidationAttributes/Validator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ValidationAttributes
+{
+    public class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            PropertyInfo[] propertyInfos = obj
+                .GetType()
+                .GetProperties()
+                .Where(x => x.GetCustomAttributes(typeof(MyValidationAttribute)).Any())
+                .ToArray();
+
+            foreach (var propertyInfo in propertyInfos)
+            {
+                var value = propertyInfo.GetValue(obj);
+                var attribute = propertyInfo.GetCustomAttribute<MyValidationAttribute>();
+
+                bool isValid = attribute.isValid(value);
+
+                if (!isValid)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
