51c70243910e892805e838ed1676781e4a297cfb
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-27 17:57:43 +0300

    Problem 02. Extended Database Solved at 10%.

--- Unit Testing - Exercise/Skeleton/DatabaseExtended.Tests/ExtendedDatabaseTests.cs
+++ Unit Testing - Exercise/Skeleton/DatabaseExtended.Tests/ExtendedDatabaseTests.cs
@@ -1,10 +1,18 @@
 namespace DatabaseExtended.Tests
 {
+    using ExtendedDatabase;
     using NUnit.Framework;
 
     [TestFixture]
     public class ExtendedDatabaseTests
     {
+        [TestCase( new Person[] {1,"Iubi" },)]
+        public void Test_If_Constructor_Create_Valid_Positiv_Data(Person[] people,int expectedCount)
+        {
+            Database database = new Database();
 
+            Assert.AreEqual(1, database.Count);
+
+        }
     }
 }
