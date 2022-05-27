03e13c55417ed072105ac59b1a904f46c5290be8
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-27 18:48:54 +0300

    Very hard

--- Unit Testing - Exercise/Skeleton/DatabaseExtended.Tests/ExtendedDatabaseTests.cs
+++ Unit Testing - Exercise/Skeleton/DatabaseExtended.Tests/ExtendedDatabaseTests.cs
@@ -16,12 +16,16 @@ namespace DatabaseExtended.Tests
 
         }
 
-        [TestCaseSource("TestCaseConstructorData")]
-        public void Test_If_Constructor_Create_Valid_Positiv_Data(Person[] people, int expectedCount)
+        [TestCaseSource("TestCaseAddData")]
+        public void Test_If_AddMethod_Create_Valid_Positiv_Data(
+            Person[] ctorPeople, Person[] peopletoAdd, int expectedCount)
         {
-            Database database = new Database(people);
+            Database database = new Database(ctorPeople);
 
-            database.Add();
+            foreach (var person in peopletoAdd)
+            {
+                database.Add(person);
+            }
 
             Assert.AreEqual(expectedCount, database.Count);
 
@@ -31,18 +35,31 @@ namespace DatabaseExtended.Tests
         {
             TestCaseData[] testCases = new TestCaseData[]
             {
-                   new TestCaseData(
+                  new TestCaseData(
                      new Person[]
                      {
-                     new Person(1, "Iubi"),
-                     new Person(2, "Kircho"),
-                     new Person(3, "Hasan"),
-                     },3),
-                   new TestCaseData(
+                         new Person(1, "Iubi"),
+                         new Person(2, "Kircho"),
+                         new Person(3, "Hasan"),
+                     },
                      new Person[]
                      {
+                         new Person(4, "Kocho"),
+                         new Person(5, "Mocho"),
+                         new Person(6, "Garabet"),
+                     },
+                     6),
+                  new TestCaseData(
+                     new Person[]
+                     {
+                     },
+                     new Person[]
+                     {
+                         new Person(1, "Koncho"),
+                         new Person(2, "Moncho"),
+                         new Person(3, "Turbalan"),
 
-                     }, 0)
+                     },3)
             };
 
             foreach (var item in testCases)
