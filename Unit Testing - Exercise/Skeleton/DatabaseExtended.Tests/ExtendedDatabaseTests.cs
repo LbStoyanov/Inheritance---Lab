f7de9643c817f964058d3dd852aa76743a0623b3
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-27 19:08:10 +0300

    Break 15 minutes,burnout!

--- Unit Testing - Exercise/Skeleton/DatabaseExtended.Tests/ExtendedDatabaseTests.cs
+++ Unit Testing - Exercise/Skeleton/DatabaseExtended.Tests/ExtendedDatabaseTests.cs
@@ -2,6 +2,7 @@ namespace DatabaseExtended.Tests
 {
     using ExtendedDatabase;
     using NUnit.Framework;
+    using System;
     using System.Collections.Generic;
 
     [TestFixture]
@@ -29,6 +30,69 @@ namespace DatabaseExtended.Tests
 
             Assert.AreEqual(expectedCount, database.Count);
 
+        }
+        [TestCaseSource("TestCaseAddInvalidData")]
+        public void Add_ShouldThrowInvalidOperationExeption_WithInvalid_Data_NegativeTest(
+            Person[] ctorPeople, Person[] peopleToAdd, int expectedCount,Person inPerson)
+        {
+            Database database = new Database(ctorPeople);
+
+            foreach (var person in peopleToAdd)
+            {
+                database.Add(person);
+            }
+
+            Assert.Throws<InvalidOperationException>(
+                () => database.Add(inPerson));
+        }
+
+        public static IEnumerable<TestCaseData> TestCaseAddInvalidData()
+        {
+            TestCaseData[] testCases = new TestCaseData[]
+            {
+                  new TestCaseData(
+                     new Person[]
+                     {
+                         new Person(1, "Iubi"),
+                         new Person(2, "Kircho"),
+                         new Person(3, "Hasan"),
+                         new Person(4, "Komara"),
+                         new Person(5, "Kompira"),
+                         new Person(6, "Kochana"),
+                         new Person(7, "Kuchkara"),
+                         new Person(8, "Kriviq"),
+                         new Person(9, "Praviq"),
+                         new Person(10, "Zabludeniq"),
+                         new Person(11, "Zaribeniq"),
+                         new Person(12, "Zaslepeniq"),
+                         new Person(11, "Zanemareniq"),
+                     },
+                     new Person[]
+                     {
+                         new Person(14, "Kocho"),
+                         new Person(15, "Mocho"),
+                         new Person(16, "Garabet"),
+                     },
+                     new Person(17,"Franco")
+                     ),
+                  new TestCaseData(
+                     new Person[]
+                     {
+                     },
+                     new Person[]
+                     {
+                         new Person(1, "Koncho"),
+                         new Person(2, "Moncho"),
+                         new Person(3, "Turbalan"),
+
+                     },
+                     new Person(7, "Turbalan"))
+            };
+
+            foreach (var item in testCases)
+            {
+                yield return item;
+            }
         }
 
         public static IEnumerable<TestCaseData> TestCaseAddData()
