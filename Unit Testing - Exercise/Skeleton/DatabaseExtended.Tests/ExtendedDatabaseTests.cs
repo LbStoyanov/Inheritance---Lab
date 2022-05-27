353c32e2c01dc9d96bcb7610786deb704eefe339
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-27 18:30:34 +0300

    Problem 02.Extended Database solved at 40%.

--- Unit Testing - Exercise/Skeleton/DatabaseExtended.Tests/ExtendedDatabaseTests.cs
+++ Unit Testing - Exercise/Skeleton/DatabaseExtended.Tests/ExtendedDatabaseTests.cs
@@ -2,17 +2,77 @@ namespace DatabaseExtended.Tests
 {
     using ExtendedDatabase;
     using NUnit.Framework;
+    using System.Collections.Generic;
 
     [TestFixture]
     public class ExtendedDatabaseTests
     {
-        [TestCase( new Person[] {1,"Iubi" },)]
+        [TestCaseSource("TestCaseConstructorData")]
         public void Test_If_Constructor_Create_Valid_Positiv_Data(Person[] people,int expectedCount)
         {
-            Database database = new Database();
+            Database database = new Database(people);
 
-            Assert.AreEqual(1, database.Count);
+            Assert.AreEqual(expectedCount, database.Count);
 
+        }
+
+        [TestCaseSource("TestCaseConstructorData")]
+        public void Test_If_Constructor_Create_Valid_Positiv_Data(Person[] people, int expectedCount)
+        {
+            Database database = new Database(people);
+
+            database.Add();
+
+            Assert.AreEqual(expectedCount, database.Count);
+
+        }
+
+        public static IEnumerable<TestCaseData> TestCaseAddData()
+        {
+            TestCaseData[] testCases = new TestCaseData[]
+            {
+                   new TestCaseData(
+                     new Person[]
+                     {
+                     new Person(1, "Iubi"),
+                     new Person(2, "Kircho"),
+                     new Person(3, "Hasan"),
+                     },3),
+                   new TestCaseData(
+                     new Person[]
+                     {
+
+                     }, 0)
+            };
+
+            foreach (var item in testCases)
+            {
+                yield return item;
+            }
+        }
+
+        public static IEnumerable<TestCaseData> TestCaseConstructorData()
+        {
+            TestCaseData[] testCases = new TestCaseData[]
+            {
+                   new TestCaseData(
+                     new Person[]
+                     {
+                     new Person(1, "Iubi"),
+                     new Person(2, "Kircho"),
+                     new Person(3, "Hasan"),
+                     },3),
+                   new TestCaseData(
+                     new Person[]
+                     {
+
+                     }, 0)
+            };
+
+            foreach (var item in testCases)
+            {
+                yield return item;
+            }
         }
     }
 }
