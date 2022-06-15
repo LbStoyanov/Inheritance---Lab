a4187e6111e6db4c022dfe206f2225a8e63abbb0
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-15 11:45:01 +0300

    Unit tests

--- OOPExamPrep - Part1/OOPExamPrep - Part1/RepairShop/RepairShop.Tests/RepairsShopTests.cs
+++ OOPExamPrep - Part1/OOPExamPrep - Part1/RepairShop/RepairShop.Tests/RepairsShopTests.cs
@@ -1,12 +1,31 @@
 using NUnit.Framework;
+using System;
 
 namespace RepairShop.Tests
 {
     public class Tests
     {
+        [TestFixture]
         public class RepairsShopTests
         {
-           
+            [Test]
+            public void Car_Creation_Working()
+            {
+                Car car = new Car("Brakma",5);
+
+                Assert.AreEqual("Brakma", car.CarModel);
+                Assert.AreEqual(5, car.NumberOfIssues);
+              
+            }
+
+            [Test]
+            public void Car_IsFixed_Working()
+            {
+                Car car = new Car("Brakma", 0);
+
+                Assert.AreEqual(true, car.IsFixed);
+            }
+
         }
     }
 }
