5f2c645433854b71c26008be00946836f4e0218a
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-18 20:55:00 +0300

    Almost done!

---
+++ OOPExamPrep -Part4/Skeleton/Gym/Models/Gyms/Gym.cs
@@ -0,0 +1,69 @@
+﻿using Gym.Models.Athletes.Contracts;
+using Gym.Models.Equipment.Contracts;
+using Gym.Models.Gyms.Contracts;
+using Gym.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Models.Gyms
+{
+    public abstract class Gym : IGym
+    {
+        private string name;
+
+        public Gym(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+        }
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidGymName);
+                }
+
+                name = value;
+            }
+        }
+
+        public int Capacity { get; private set; }
+            
+
+        public double EquipmentWeight { get; private set; }
+           
+
+        public ICollection<IEquipment> Equipment { get; }
+
+        public ICollection<IAthlete> Athletes { get; }
+
+        public void AddAthlete(IAthlete athlete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddEquipment(IEquipment equipment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exercise()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GymInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveAthlete(IAthlete athlete)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
