2fba544461b62eb147965267cd65b1405ba439df
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-20 19:54:01 +0300

    ExamPrepOOP-Planet

---
+++ OOPExamPrep -Part5/SpaceStation/Models/Astronauts/Astronaut.cs
@@ -0,0 +1,38 @@
+﻿using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Bags.Contracts;
+using SpaceStation.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Astronauts
+{
+    public abstract class Astronaut : IAstronaut
+    {
+        private string name;
+        public string Name
+        {
+            get { return this.name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(ExceptionMessages.InvalidAstronautName);
+                }
+
+                this.name = value;
+            }
+        }
+
+        public double Oxygen => throw new NotImplementedException();
+
+        public bool CanBreath => throw new NotImplementedException();
+
+        public IBag Bag => throw new NotImplementedException();
+
+        public void Breath()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
