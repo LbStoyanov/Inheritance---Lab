14b9661d995f51df0c4945a7dba4a62ec4e60bc5
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-20 20:40:54 +0300

    PlanetStation structure 50/50 points.Done!

---
+++ OOPExamPrep -Part5/SpaceStation/Models/Mission/Mission.cs
@@ -0,0 +1,17 @@
+﻿using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Mission.Contracts;
+using SpaceStation.Models.Planets.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Mission
+{
+    public class Mission : IMission
+    {
+        public void Explore(IPlanet planet, ICollection<IAstronaut> astronauts)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OOPExamPrep -Part5/SpaceStation/Repositories/AstronautRepository.cs
@@ -0,0 +1,34 @@
+﻿using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Repositories
+{
+    internal class AstronautRepository : IRepository<IAstronaut>
+    {
+        private List<IAstronaut> astronauts;
+
+        public AstronautRepository()
+        {
+            this.astronauts = new List<IAstronaut>();
+        }
+        public IReadOnlyCollection<IAstronaut> Models { get { return this.astronauts.AsReadOnly(); } }
+
+        public void Add(IAstronaut model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAstronaut FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(IAstronaut model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
