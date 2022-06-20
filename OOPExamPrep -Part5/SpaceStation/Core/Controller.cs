b0e06a3c6a13edcbb45eb9964e40c9e903244975
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-20 21:08:00 +0300

    Done for today!

---
+++ OOPExamPrep -Part5/SpaceStation/Core/Controller.cs
@@ -0,0 +1,51 @@
+﻿using SpaceStation.Core.Contracts;
+using SpaceStation.Models.Astronauts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Core
+{
+    public class Controller : IController
+    {
+        public string AddAstronaut(string type, string astronautName)
+        {
+            if (type == typeof(Biologist).ToString())
+            {
+
+            }
+            else if (type == typeof(Geodesist).ToString())
+            {
+
+            }
+            else if (type == typeof(Meteorologist).ToString())
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+
+        public string AddPlanet(string planetName, params string[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ExplorePlanet(string planetName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Report()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RetireAstronaut(string astronautName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
