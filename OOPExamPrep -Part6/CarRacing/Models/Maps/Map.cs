9bba57d3386da1ca517cdf21c38ddb571f8129ac
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-22 21:09:11 +0300

    ReposCreation

---
+++ OOPExamPrep -Part6/CarRacing/Models/Maps/Map.cs
@@ -0,0 +1,33 @@
+﻿using CarRacing.Models.Maps.Contracts;
+using CarRacing.Models.Racers.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Models.Maps
+{
+    public class Map : IMap
+    {
+        public string StartRace(IRacer racerOne, IRacer racerTwo)
+        {
+            double racingBehaviorMultiplier = 0.0;
+
+            string result = string.Empty;
+
+            if (!racerOne.IsAvailable() && racerTwo.IsAvailable())
+            {
+                racerTwo.Car.Drive();
+            }
+            else if (racerOne.IsAvailable() && !racerTwo.IsAvailable())
+            {
+                racerOne.Car.Drive();
+            }
+            else if (!racerOne.IsAvailable() && !racerTwo.IsAvailable())
+            {
+
+            }
+
+            return result.TrimEnd();
+        }
+    }
+}
