643f837c61a8b05fb9ca93655b5629b46101fa6f
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-24 15:19:41 +0300

    UnitTestingRobotManager

--- OOPExamPrep -Part6/Robots.Tests/RobotsTests.cs
+++ OOPExamPrep -Part6/Robots.Tests/RobotsTests.cs
@@ -1,8 +1,26 @@
-﻿namespace Robots.Tests
+﻿using NUnit.Framework;
+using Robots;
+using System;
+public class RobotsTests
 {
-    using System;
-
-    public class RobotsTests
+    [Test]
+    public void Robot_Creation_Working()
     {
+        Robot robot = new Robot("Chicho Mitko", 10);
+
+        Assert.AreEqual("Chicho Mitko", robot.Name);
+        Assert.AreEqual(10, robot.Battery);
+        Assert.AreEqual(10, robot.MaximumBattery);
     }
+
+    [Test]
+    public void RobotManager_Creation_Woking()
+    {
+        RobotManager robotManager = new RobotManager(10);
+
+        Assert.AreEqual(10, robotManager.Capacity);
+        Assert.AreEqual(0, robotManager.Count);
+    }
+
 }
+
