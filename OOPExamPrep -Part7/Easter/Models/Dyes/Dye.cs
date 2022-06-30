ec0bd5d45add5dfa511465f8856ce47bd414eb8d
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-30 20:18:02 +0300

    Eastern Problem - 01. Structure 50/50 FinalScore

---
+++ OOPExamPrep -Part7/Easter/Models/Dyes/Dye.cs
@@ -0,0 +1,34 @@
+﻿using Easter.Models.Dyes.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easter.Models.Dyes
+{
+    
+    public class Dye : IDye
+    {
+        //private int power;
+        public Dye(int power)
+        {
+            this.Power = power;
+        }
+        public int Power { get; private set; }
+        
+
+        public bool IsFinished()
+        {
+            return this.Power == 0;
+        }
+
+        public void Use()
+        {
+            this.Power -= 10;
+
+            if (this.Power < 0)
+            {
+                this.Power = 0;
+            }
+        }
+    }
+}
---
+++ OOPExamPrep -Part7/Easter/Models/Eggs/Egg.cs
@@ -0,0 +1,43 @@
+﻿using Easter.Models.Eggs.Contracts;
+using Easter.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easter.Models.Eggs
+{
+    public class Egg : IEgg
+    {
+        private string name;
+        //private int energyRequired;
+        public Egg(string name, int energyRequired)
+        {
+            this.Name = name;
+            this.EnergyRequired = energyRequired;
+        }
+        public string Name
+        {
+            get { return this.name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidEggName);
+                }
+            }
+        }
+
+        public int EnergyRequired { get; private set; }
+
+
+        public void GetColored()
+        {
+            this.EnergyRequired -= 10;
+        }
+
+        public bool IsDone()
+        {
+            return this.EnergyRequired == 0;
+        }
+    }
+}
