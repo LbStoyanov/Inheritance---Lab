55f31daaac89ac0fc800bcae8acb2ede025d4bf7
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-01 09:13:31 +0300

    S

--- OOPExamPrep -Part7/Easter/Core/Controller.cs
+++ OOPExamPrep -Part7/Easter/Core/Controller.cs
@@ -1,6 +1,7 @@
 ﻿using Easter.Core.Contracts;
 using Easter.Models.Bunnies;
 using Easter.Models.Dyes;
+using Easter.Models.Eggs;
 using Easter.Repositories;
 using Easter.Utilities.Messages;
 using System;
@@ -13,9 +14,11 @@ namespace Easter.Core
     public class Controller : IController
     {
         BunnyRepository bunnies;
+        EggRepository eggs;
         public Controller()
         {
             this.bunnies = new BunnyRepository();
+            this.eggs = new EggRepository();
         }
         public string AddBunny(string bunnyType, string bunnyName)
         {
@@ -57,12 +60,23 @@ namespace Easter.Core
 
         public string AddEgg(string eggName, int energyRequired)
         {
-            throw new NotImplementedException();
+            Egg egg = new Egg(eggName, energyRequired);
+            this.eggs.Add(egg);
+
+            return String.Format(OutputMessages.EggAdded, eggName);
+
         }
 
         public string ColorEgg(string eggName)
         {
-            throw new NotImplementedException();
+            var coloringBunnies = bunnies.Models.Select(x => x.Energy >= 50).ToList();
+
+            if (coloringBunnies.Count == 0)
+            {
+                throw new InvalidOperationException(ExceptionMessages.BunniesNotReady);
+            }
+
+            Egg egg = (Egg)eggs.FindByName(eggName);
         }
 
         public string Report()
