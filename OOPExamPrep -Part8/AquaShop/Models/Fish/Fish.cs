5a0ad226b2e3c101b854b25d3b5fc2904ce4952f
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-14 17:46:57 +0300

    Aquarium exam - OOP C#

---
+++ OOPExamPrep -Part8/AquaShop/Models/Fish/Fish.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AquaShop.Models.Fish.Contracts;
+
+namespace AquaShop.Models.Fish
+{
+    public abstract class Fish : IFish
+    {
+        public string Name { get; }
+        public string Species { get; }
+        public int Size { get; }
+        public decimal Price { get; }
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
