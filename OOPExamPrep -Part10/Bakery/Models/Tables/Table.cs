916970f505fb0df505ace1bd00d4e6c212232e50
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-29 19:38:58 +0300

    BakeryTask - OOPExam

---
+++ OOPExamPrep -Part10/Bakery/Models/Tables/Table.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Bakery.Models.BakedFoods.Contracts;
+using Bakery.Models.Drinks.Contracts;
+using Bakery.Models.Tables.Contracts;
+
+namespace Bakery.Models.Tables
+{
+    public abstract class Table : ITable
+    {
+
+        public Table()
+        {
+            
+        }
+        public int TableNumber { get; }
+        public int Capacity { get; }
+        public int NumberOfPeople { get; }
+        public decimal PricePerPerson { get; }
+        public bool IsReserved { get; }
+        public decimal Price { get; }
+        public void Reserve(int numberOfPeople)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OrderFood(IBakedFood food)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OrderDrink(IDrink drink)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetBill()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFreeTableInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
