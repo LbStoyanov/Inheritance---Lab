addd5b18d1fa08a3cbfddb7c490ae5071966a2dd
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-09 13:13:04 +0300

    Structure

---
+++ OOPExamPrep -Part11/Application/Exam-Skeleton/EasterRaces/Models/Cars/Entities/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EasterRaces.Models.Cars.Contracts;
+using EasterRaces.Utilities.Messages;
+
+namespace EasterRaces.Models.Cars.Entities
+{
+    public abstract class Car : ICar
+    {
+        private string model;
+        private int horsePower;
+        private int minHorsePower;
+        private int maxHorsePower;
+
+        public Car(string model, int horsePower, double cubicCentimeters, int minHorsePower, int maxHorsePower)
+        {
+            this.Model = model;
+            this.HorsePower = horsePower;
+            this.CubicCentimeters = cubicCentimeters;
+            this.minHorsePower = minHorsePower;
+            this.maxHorsePower = maxHorsePower;
+
+        }
+
+        public string Model
+        {
+            get => this.model;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) && value.Length < 4)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidModel, this.model, value.Length));
+                }
+
+                this.model = value;
+            }
+        }
+
+        public int HorsePower
+        {
+            get => this.horsePower;
+            protected set
+            {
+                if (value < this.minHorsePower && value > this.maxHorsePower)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidHorsePower, value.ToString());
+                }
+            }
+        }
+        public double CubicCentimeters { get; protected set; }
+        public double CalculateRacePoints(int laps)
+        {
+            return (this.CubicCentimeters / this.horsePower) * laps;
+        }
+    }
+}
---
+++ OOPExamPrep -Part11/Application/Exam-Skeleton/EasterRaces/Models/Drivers/Entities/Driver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EasterRaces.Models.Cars.Contracts;
+using EasterRaces.Models.Drivers.Contracts;
+using EasterRaces.Utilities.Messages;
+
+namespace EasterRaces.Models.Drivers.Entities
+{
+    public  class Driver :IDriver
+    {
+        private string name;
+        private bool carParticipate = false;
+        public Driver()
+        {
+            
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value) && value.Length < 5)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidName, value,value.Length));
+                }
+
+                this.name = value;
+            }
+        }
+        public ICar Car { get; private set; }
+        public int NumberOfWins { get; private set; }
+
+        public bool CanParticipate
+        {
+            get { return this.carParticipate; }
+        }
+       
+        public void WinRace()
+        {
+            this.NumberOfWins++;
+        }
+
+        public void AddCar(ICar car)
+        {
+            if (car == null)
+            {
+                throw new ArgumentNullException(ExceptionMessages.CarInvalid);
+            }
+
+            this.Car = car;
+            this.carParticipate = true;
+        }
+    }
+}
