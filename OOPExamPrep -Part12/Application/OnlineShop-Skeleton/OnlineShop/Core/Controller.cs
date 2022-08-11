5255eebdb662429fa61c4ede4892dfcb789eed8f
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-11 13:48:54 +0300

    A

--- OOPExamPrep -Part12/Application/OnlineShop-Skeleton/OnlineShop/Core/Controller.cs
+++ OOPExamPrep -Part12/Application/OnlineShop-Skeleton/OnlineShop/Core/Controller.cs
@@ -1,51 +1,215 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using OnlineShop.Common.Constants;
+using OnlineShop.Models.Products.Components;
+using OnlineShop.Models.Products.Computers;
+using OnlineShop.Models.Products.Peripherals;
 
 namespace OnlineShop.Core
 {
     public class Controller : IController
     {
+        private  List<IComputer> computers;
+        private  List<IComponent> components;
+        private  List<IPeripheral> peripherals;
+
+        public Controller()
+        {
+            this.computers = new List<IComputer>();
+            this.components = new List<IComponent>();
+            this.peripherals = new List<IPeripheral>();
+        }
         public string AddComputer(string computerType, int id, string manufacturer, string model, decimal price)
         {
-            throw new NotImplementedException();
+
+            IsComputerIdExist(id);
+
+            if (computerType != "DesktopComputer" && computerType != "Laptop")
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidComputerType);
+            }
+            IComputer computer = null;
+
+            if (computerType == "DesktopComputer")
+            {
+                computer = new DesktopComputer(id, manufacturer, model, price);
+            }
+            else if(computerType == "Laptop")
+            {
+                computer = new Laptop(id, manufacturer, model, price);
+            }
+
+            this.computers.Add(computer);
+
+            return string.Format(SuccessMessages.AddedComputer, id);
+            
         }
 
         public string AddPeripheral(int computerId, int id, string peripheralType, string manufacturer, string model, decimal price,
             double overallPerformance, string connectionType)
         {
-            throw new NotImplementedException();
+            IsComputerIdExist(computerId);
+
+            IComputer computer = this.computers.FirstOrDefault(x => x.Id == computerId);
+
+            if (this.peripherals.Any(x => x.Id == id))
+            {
+                throw new ArgumentException(ExceptionMessages.ExistingPeripheralId);
+            }
+
+            IPeripheral peripheral = null;
+
+            if (peripheralType == "Keyboard")
+            {
+
+                peripheral = new Keyboard(id, manufacturer, model, price, overallPerformance, connectionType);
+            }
+            else if (peripheralType == "Headset")
+            {
+                peripheral = new Headset(id, manufacturer, model, price, overallPerformance, connectionType);
+            }
+            else if (peripheralType == "Monitor")
+            {
+                peripheral = new Monitor(id, manufacturer, model, price, overallPerformance, connectionType);
+            }
+            else if (peripheralType == "Mouse")
+            {
+                peripheral = new Mouse(id, manufacturer, model, price, overallPerformance, connectionType);
+            }
+            else
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidPeripheralType);
+            }
+
+            this.peripherals.Add(peripheral);
+            computer!.AddPeripheral(peripheral);
+
+            return string.Format(SuccessMessages.AddedPeripheral, peripheralType, id, computerId);
         }
 
         public string RemovePeripheral(string peripheralType, int computerId)
         {
-            throw new NotImplementedException();
+            IsComputerIdExist(computerId);
+
+            IComputer computer = this.computers.FirstOrDefault(x => x.Id == computerId);
+
+            IPeripheral peripheral = this.peripherals.FirstOrDefault(x => x.GetType().Name == peripheralType);
+
+            if (peripheral != null)
+            {
+                computer.RemovePeripheral(peripheralType);
+                return $"Successfully removed {peripheralType} with id {peripheral.Id}.";
+            }
+
+            return "";
         }
 
         public string AddComponent(int computerId, int id, string componentType, string manufacturer, string model, decimal price,
             double overallPerformance, int generation)
         {
-            throw new NotImplementedException();
+            //IsComputerIdExist(computerId);
+
+            IComputer computer = this.computers.FirstOrDefault(computer => computer.Id == computerId);
+
+            if (computer!.Components.Any(x=> x.Id == id))
+            {
+                throw new ArgumentException(ExceptionMessages.ExistingComponentId);
+            }
+
+            IComponent component = null;
+
+            if (componentType == "CentralProcessingUnit")
+            {
+                component = new CentralProcessingUnit(id, manufacturer, model, price, overallPerformance, generation);
+            }
+            else if (componentType == "Motherboard")
+            {
+                component = new Motherboard(id, manufacturer, model, price, overallPerformance, generation);
+            }
+            else if (componentType == "PowerSupply")
+            {
+                component = new PowerSupply(id, manufacturer, model, price, overallPerformance, generation);
+            }
+            else if (componentType == "RandomAccessMemory")
+            {
+                component = new RandomAccessMemory(id, manufacturer, model, price, overallPerformance, generation);
+            }
+            else if (componentType == "SolidStateDrive")
+            {
+                component = new SolidStateDrive(id, manufacturer, model, price, overallPerformance, generation);
+            }
+            else if (componentType == "VideoCard")
+            {
+                component = new VideoCard(id, manufacturer, model, price, overallPerformance, generation);
+            }
+            else
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidComponentType);
+            }
+            this.components.Add(component);
+            computer.AddComponent(component);
+
+            return string.Format(SuccessMessages.AddedComponent, componentType, id, computerId);
+            
         }
 
         public string RemoveComponent(string componentType, int computerId)
         {
-            throw new NotImplementedException();
+            IsComputerIdExist(computerId);
+
+            IComputer computer = this.computers.FirstOrDefault(x => x.Id == computerId);
+
+            IComponent component = this.components.FirstOrDefault(x => x.GetType().Name == componentType);
+
+            return computer.RemoveComponent(componentType).ToString();
+
         }
 
         public string BuyComputer(int id)
         {
-            throw new NotImplementedException();
+            IsComputerIdExist(id);
+
+            IComputer computer = this.computers.FirstOrDefault(x => x.Id == id);
+
+            this.computers.Remove(computer);
+
+            return computer!.ToString();
         }
 
         public string BuyBest(decimal budget)
         {
-            throw new NotImplementedException();
+            var best = this.computers.OrderByDescending(x => x.OverallPerformance).Max();
+
+            if (best.Price <= budget)
+            {
+                throw new ArgumentException($" Can't buy a computer with a budget of ${budget}.");
+            }
+
+            this.computers.Remove(best);
+
+            return best.ToString();
+            
         }
 
         public string GetComputerData(int id)
         {
-            throw new NotImplementedException();
+            IsComputerIdExist(id);
+
+            var computer = this.computers.FirstOrDefault(x => x.Id == id);
+
+            return computer.ToString();
         }
+
+        private void IsComputerIdExist(int id)
+        {
+            if (this.computers.Any(x => x.Id == id))
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.ExistingComputerId, id));
+            }
+        }
+
+
     }
 }
--- OOPExamPrep -Part12/Application/OnlineShop-Skeleton/OnlineShop/Models/Products/Computers/Computer.cs
+++ OOPExamPrep -Part12/Application/OnlineShop-Skeleton/OnlineShop/Models/Products/Computers/Computer.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using OnlineShop.Common.Constants;
 using OnlineShop.Models.Products.Components;
 using OnlineShop.Models.Products.Peripherals;
 
@@ -8,36 +11,73 @@ namespace OnlineShop.Models.Products.Computers
 {
     public abstract class Computer : Product,IComputer
     {
+        private readonly List<IComponent> components;
+        private readonly List<IPeripheral> peripherals;
         protected Computer(int id, string manufacturer, string model, decimal price, double overallPerformance) 
             : base(id, manufacturer, model, price, overallPerformance)
         {
+            this.components = new List<IComponent>();
+            this.peripherals = new List<IPeripheral>();
         }
 
-        public IReadOnlyCollection<IComponent> Components { get; }
-        public IReadOnlyCollection<IPeripheral> Peripherals { get; }
+        public IReadOnlyCollection<IComponent> Components => this.components.AsReadOnly();
+        public IReadOnlyCollection<IPeripheral> Peripherals => this.peripherals.AsReadOnly();
         public void AddComponent(IComponent component)
         {
-            throw new NotImplementedException();
+            if (this.components.Any(x => x.GetType().Name == component.GetType().Name))
+            {
+                throw new ArgumentException(
+                    string.Format(
+                        ExceptionMessages.InvalidComponentType,
+                        component.GetType().Name,this.GetType().Name,this.Id));
+            }
+
+            this.components.Add(component);
         }
 
         public IComponent RemoveComponent(string componentType)
         {
-            throw new NotImplementedException();
+            if (this.components.Count == 0 || this.Components.All(x => x.GetType().Name != componentType))
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.NotExistingComponent,componentType,this.GetType().Name,this.Id));
+            }
+
+            IComponent component = this.components.FirstOrDefault(x => x.GetType().Name == componentType);
+
+            this.components.Remove(component);
+
+            return component;
         }
 
         public void AddPeripheral(IPeripheral peripheral)
         {
-            throw new NotImplementedException();
+            if (this.peripherals.Contains(peripheral))
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.ExistingPeripheral,
+                    peripheral.GetType().Name, this.GetType().Name, this.Id));
+            }
+
+            this.peripherals.Add(peripheral);
         }
 
         public IPeripheral RemovePeripheral(string peripheralType)
         {
-            throw new NotImplementedException();
+            if (this.components.Count == 0 || this.Components.All(x => x.GetType().Name != peripheralType))
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.NotExistingPeripheral, peripheralType, this.GetType().Name, this.Id));
+            }
+
+            IPeripheral peripheral = this.peripherals.FirstOrDefault(x => x.GetType().Name == peripheralType);
+
+            this.peripherals.Remove(peripheral);
+
+            return peripheral;
         }
 
         public override string ToString()
         {
-            return base.ToString() + this.Components.ToString() + this.Peripherals.ToString();
+            //May need use Foreach for every component and peripheral!!!
+            return base.ToString() + (string.Format(SuccessMessages.ComputerComponentsToString,this.Components)) + this.Peripherals.ToString();
         }
     }
 }
