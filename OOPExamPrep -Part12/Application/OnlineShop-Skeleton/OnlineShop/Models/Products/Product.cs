dd6d0976ba43eefa1ace04fa54ff6d9f6a3cfef6
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-11 10:14:57 +0300

    ApplicationStructure

---
+++ OOPExamPrep -Part12/Application/OnlineShop-Skeleton/OnlineShop/Models/Products/Product.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnlineShop.Common.Constants;
+
+namespace OnlineShop.Models.Products
+{
+    public abstract class Product :IProduct
+    {
+        private int id;
+        private string manufacturer;
+        private string model;
+        private decimal price;
+        private double overallPerformance;
+        protected Product()
+        {
+                
+        }
+
+        public int Id
+        {
+            get => this.id;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidProductId);
+                }
+                this.id = value;
+            }
+        }
+
+        public string Manufacturer
+        {
+            get => this.manufacturer;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidManufacturer);
+                }
+
+                this.manufacturer = value;
+            }
+        }
+
+        public string Model
+        {
+            get => this.model;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidModel);
+                }
+
+                this.model = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get => this.price;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidPrice);
+                }
+                this.price = value;
+            }
+        }
+
+        public double OverallPerformance
+        {
+            get => this.overallPerformance;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidOverallPerformance);
+                }
+                this.overallPerformance = value;
+            }
+        }
+    }
+}
